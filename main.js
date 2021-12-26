feb820fbf6dde052bab4c7602d1640a650bd4fb1
sajadpp120 <96676224+sajadpp120@users.noreply.github.com>
2021-12-26 13:50:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -61,3 +61,8 @@ searchBtn.addEventListener('click', () => {
     }
 })
 
+const menu = document.querySelector('#menu');
+const sidebar = document.querySelector('.sidebar');
+menu.addEventListener('click', function() {
+    sidebar.classList.toggle('show-sidebar');
+})
